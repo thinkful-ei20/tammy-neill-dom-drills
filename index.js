1e42a56064ef84e7445243d68c473996fc81a4c6
neillsom <neillsom@mail.com>
2018-04-05 11:43:52 -0700

    completed fizzbuzz

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-'use strict'
+'use strict';
 
-function fizzBuzz(num) {
-  if (num % 5 === 0 && num % 3 === 0) {
-    return 'fizzbuzz';
-  } else if (num % 5 === 0) {
-    return 'buzz';
-  } else if (num % 3 === 0) {
-    return 'fizz';
-  } 
+console.log('test');
+
+function fizzbuzz(num) {
+  if (num % 15 === 0) return 'fizzbuzz';
+  if (num % 5 === 0) return 'buzz';
+  if (num % 3 === 0) return 'fizz';
   return num;
 }
 
 function fizzbuzzer(total) {
   const results = [];
-  for (let i = 1; i <= total; i ++) {
-   results.push(fizzBuzz(i));
+  for (let i = 1; i <= total; i++) {
+    results.push(fizzbuzz(i));
   }
+  return results;
 }
 
-
-const html = resultsToHtml(fizzResults);
- return results.map(number => {
-   
- }
-  
-  
-  // array of divs
-$('.js-resuls').append(html);
-
-//check for DOM ready
-function main() {
-  console.log('DOM is ready');
-
-//get value of submit
-
-function handleSubmit(){
-    $('form').on('submit', event => {
-    event.preventDefault();
-    const value = $('#number-choice').val();
-    const fizzResults = fizzbuzzer(number);
-    // gives array of numbers [1, 2, 'fizz']
+function resultsToHtml(results) {
+  return results.map(result => {
+    let fizzBuzzClass = '';
+    if (typeof result === 'string') {
+      fizzBuzzClass = result;
     }
-    
-});
+
+    return `
+      <div class="fizz-buzz-item ${fizzBuzzClass}">
+        <span>${result}</span>
+      </div>    
+    `;
+  });
+}
+
+function main() {
+  // When DOM ready...
+  $('form').on('submit', event => {
+    event.preventDefault();
+    const number = $('#number-choice').val();
+    const fizzResults = fizzbuzzer(number);
+    // [ 1, 2, 'fizz', 4, 'buzz' ]
+    const html = resultsToHtml(fizzResults);
+    // [ '<div>1</div>', '<div>fizz</div>' ]
+    $('.js-results').html(html);
+    $('#number-choice').val('');
+  });
 
 }
+
+$(main);
